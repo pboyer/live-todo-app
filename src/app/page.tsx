4cf2961e359bc0ce73baddc53d6a826c4bbbe054
Peter Boyer <peter.b.boyer@gmail.com>
2023-11-28 22:39:38 -0500

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,166 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useState } from "react";
+import { createClient } from "@liveblocks/client";
+import { liveblocksEnhancer } from "@liveblocks/redux";
+import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
+import { Provider, shallowEqual, useDispatch, useSelector } from "react-redux";
+import { actions } from "@liveblocks/redux";
+import { nanoid } from "nanoid";
+
+const client = createClient({
+  publicApiKey: "pk_test_YQwmruaR10F3T86-tzbm-o-r",
+});
+
+interface Todo {
+  id: string;
+  todo: string;
+  sortIndex: number;
+}
+
+interface TodoState {
+  draft: string;
+  isTyping: boolean;
+  todos: { [id: string]: Todo };
+}
+
+const initialState: TodoState = {
+  draft: "",
+  isTyping: false,
+  todos: {},
+};
+
+const slice = createSlice({
+  name: "state",
+  initialState,
+  reducers: {
+    setDraft: (state, action: PayloadAction<string>) => {
+      state.isTyping = action.payload === "" ? false : true;
+      state.draft = action.payload;
+    },
+    addTodo: (state, action: PayloadAction<string>) => {
+      const id = nanoid();
+      state.todos[id] = {
+        id,
+        todo: action.payload,
+        sortIndex:
+          Math.max(...Object.values(state.todos).map((x) => x.sortIndex)) + 1,
+      };
+    },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      delete state.todos[action.payload];
+    },
+  },
+});
+
+export function makeStore() {
+  return configureStore({
+    reducer: slice.reducer,
+    enhancers: [
+      liveblocksEnhancer({
+        client,
+        storageMapping: { todos: true },
+        presenceMapping: { isTyping: true } as any,
+      }),
+    ],
+  });
+}
+
+const store = makeStore();
+
+function WhoIsHere() {
+  const othersUsersCount: any = useSelector(
+    (state: { liveblocks: { others: any[] } }) => state.liveblocks.others.length
+  );
+
+  return (
+    <div className="dark:text-slate-500 text-slate-600 text-xs p-1 font-light flex justify-between">
+      <SomeoneIsTyping />
+      <div>There are {othersUsersCount} other users online</div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Provider store={store}>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="w-full  m-4">
+          <Inner />
         </div>
+      </main>
+    </Provider>
+  );
+}
+
+function SomeoneIsTyping() {
+  const others = useSelector((state: any) => state.liveblocks.others);
+
+  return others.some((user: any) => user.presence?.isTyping) ? (
+    <div className="someone_is_typing">Someone is typing</div>
+  ) : (
+    <div></div>
+  );
+}
+
+function Inner() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.enterRoom("redux-demo-room"));
+
+    return () => {
+      dispatch(actions.leaveRoom());
+    };
+  }, [dispatch]);
+
+  const { todos, draft } = useSelector(
+    (state: TodoState) => ({
+      todos: state.todos,
+      draft: state.draft,
+    }),
+    shallowEqual
+  );
+
+  const sorted = Object.values(todos).sort((a, b) => a.sortIndex - b.sortIndex);
+
+  return (
+    <>
+      <WhoIsHere />
+
+      <input
+        type="text"
+        className="dark:bg-slate-800 w-full dark:text:white outline-none p-4 focus:color rounded drop-shadow-md"
+        placeholder="Add a new thing to do"
+        value={draft}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            dispatch(slice.actions.addTodo(draft));
+            dispatch(slice.actions.setDraft(""));
+          }
+        }}
+        onChange={(e) => {
+          dispatch(slice.actions.setDraft(e.currentTarget.value));
+        }}
+      ></input>
+
+      <div className="flex flex-col m-4">
+        {sorted.map((todo) => {
+          return (
+            <div
+              className="dark:text-slate-300 text-slate-600 flex justify-between space-between align-items mt-4"
+              key={todo.id}
+            >
+              <div>{todo.todo}</div>
+              <button
+                onClick={() => dispatch(slice.actions.deleteTodo(todo.id))}
+              >
+                &#x2715;
+              </button>
+            </div>
+          );
+        })}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
